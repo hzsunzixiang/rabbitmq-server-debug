5e90be674c5b3878653513d494831a2c9f28faa7
haichengsun <haichengsun123@163.com>
2023-10-04 23:21:05 -0400

    erlang systemd

--- book/erlang_systemd/src/erlang_systemd_sup.erl
+++ book/erlang_systemd/src/erlang_systemd_sup.erl
@@ -8,5 +8,12 @@ start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [],
-	{ok, {{one_for_one, 1, 5}, Procs}}.
+    SupFlags = #{strategy => one_for_one,
+                 intensity => 5,
+                 period => 3600},
+    ChildSpecs = [child_static(frequency)],
+    {ok, {SupFlags, ChildSpecs}}.
+
+child_static(Module) ->
+    {Module, {Module, start_link, []},
+     permanent, 5000, worker, [Module]}.
