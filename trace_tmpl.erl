35e3e4cadb0c77417af617602d8f2bd523a37a50
haichengsun <haichengsun123@163.com>
2023-10-25 08:57:03 -0400

    commit

--- trace_tmpl.erl
+++ trace_tmpl.erl
@@ -4,7 +4,7 @@ f().
 LogFileName = "/trace_prelaunch1025.1.txt".
 LogDir = CurrentDir ++ "/trace_log".
 %LogPath="/home/ericksun/program/rabbitmq-server-debug/rabbitmq_prelaunch-sup-dist/trace_log/".
-LogNum=50000,
+LogNum=500000,
 LP = fun() -> [code:ensure_loaded(list_to_atom(filename:rootname(filename:basename(F)))) || P <- code:get_path(), F <- filelib:wildcard(P ++ "/*.beam")] end.
 LP().  %% sync
 FileName= string:concat(LogDir, LogFileName).  %LogPath = LogDir ++ LogFileName.
@@ -18,19 +18,18 @@ DirDel=[aten, base64url, credentials_obfuscation, cuttlefish, enough, gen_batch_
 DirsNoPrefix=DirTmp--DirDel.
 
 {ok, CurrentDir} = file:get_cwd().
-Prefix = CurrentDir ++ "/plugins/".
-Dirs2 = [filelib:wildcard(Prefix++atom_to_list(P)++"-*") || P <- DirsNoPrefix].
-
+Prefix = CurrentDir ++ "/deps/".
+Dirs = [Prefix++atom_to_list(P) || P <- DirsNoPrefix].
 %% the rabbitmq_prelaunch
 %% Prelaunch = CurrentDir.
-Kernel = "/home/ericksun/install/otp-26.1/lib/erlang/lib/kernel-9.1".
+%Kernel = "/home/ericksun/install/otp-26.1/lib/erlang/lib/kernel-9.1".
 %Dirs2=[Prelaunch | Dirs1].
-Dirs=[Dirs2 | [Kernel]].
+%Dirs=[Dirs2 | [Kernel]].
 %Dirs=[Prelaunch | Dirs1 ].
 ListModAll=[list_to_atom(filename:rootname(filename:basename(F))) || P <- Dirs, F <- filelib:wildcard(P ++ "/ebin/*.beam")].
 %ListModeAdd=[logger_backend,logger,logger_config,logger_disk_log_h,logger_filters,logger_formatter,logger_handler_watcher,logger_h_common,logger_olp,logger_proxy,logger_server,logger_simple_h,logger_std_h,logger_sup].
 %ListModeAdd=[logger,logger_disk_log_h,logger_filters,logger_formatter,logger_h_common,logger_simple_h,logger_std_h].
-ListModeAdd=[net_adm].
+ListModeAdd=[].
 
 ListMod=ListModAll++ListModeAdd.
 %lists:member(rabbit_env,  ListMod).
