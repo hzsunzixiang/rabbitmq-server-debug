b0f1f53838aa0e785c1148e2bb85295def308bd8
haichengsun <haichengsun123@163.com>
2023-10-26 05:01:20 -0400

    trace debug

--- rabbitmq-server-v3.12.4/trace_plugins.erl
+++ rabbitmq-server-v3.12.4/trace_plugins.erl
@@ -1,7 +1,7 @@
 f().
 %% load all the modules
 {ok, CurrentDir} = file:get_cwd().
-LogFileName = "/trace_prelaunch1025.1.txt".
+LogFileName = "/trace_prelaunch1026.1.txt".
 LogDir = CurrentDir ++ "/trace_log".
 %LogPath="/home/ericksun/program/rabbitmq-server-debug/rabbitmq_prelaunch-sup-dist/trace_log/".
 LogNum=50000,
@@ -11,9 +11,9 @@ FileName= string:concat(LogDir, LogFileName).  %LogPath = LogDir ++ LogFileName.
 file:delete(FileName).
 file:make_dir(LogDir).
 
-DirTmp=[aten, base64url, credentials_obfuscation, cuttlefish, enough, gen_batch_server, getopt, observer_cli, osiris, rabbit, rabbit_common, rabbitmq_codegen, ranch, recon, seshat, syslog, sysmon_handler, systemd, thoas].
+DirTmp=[aten, base64url, credentials_obfuscation, cuttlefish, enough, gen_batch_server, getopt, observer_cli, osiris, rabbit, rabbit_common, rabbitmq_codegen, ranch, recon, seshat, syslog, sysmon_handler, systemd, thoas, rabbitmq_prelaunch].
 
-DirDel=[aten, base64url, credentials_obfuscation, cuttlefish, enough, gen_batch_server, getopt, observer_cli, osiris, rabbitmq_codegen, ranch, recon, seshat, syslog, sysmon_handler, systemd, thoas].
+DirDel=[aten, base64url, credentials_obfuscation, cuttlefish, enough, gen_batch_server, getopt, observer_cli, osiris, rabbitmq_codegen, ranch, recon, seshat, syslog, sysmon_handler, systemd, thoas, rabbitmq_prelaunch].
 
 DirsNoPrefix=DirTmp--DirDel.
 
@@ -23,7 +23,7 @@ Dirs = [filelib:wildcard(Prefix++atom_to_list(P)++"-*") || P <- DirsNoPrefix].
 
 %% the rabbitmq_prelaunch
 %% Prelaunch = CurrentDir.
-Kernel = "/home/ericksun/install/otp-26.1/lib/erlang/lib/kernel-9.1".
+%Kernel = "/home/ericksun/install/otp-26.1/lib/erlang/lib/kernel-9.1".
 %Dirs2=[Prelaunch | Dirs1].
 %Dirs=[Dirs2 | [Kernel]].
 %Dirs=[Prelaunch | Dirs1 ].
@@ -31,8 +31,9 @@ ListModAll=[list_to_atom(filename:rootname(filename:basename(F))) || P <- Dirs, 
 %ListModeAdd=[logger_backend,logger,logger_config,logger_disk_log_h,logger_filters,logger_formatter,logger_handler_watcher,logger_h_common,logger_olp,logger_proxy,logger_server,logger_simple_h,logger_std_h,logger_sup].
 %ListModeAdd=[logger,logger_disk_log_h,logger_filters,logger_formatter,logger_h_common,logger_simple_h,logger_std_h].
 ListModeAdd=[net_adm].
+ListModeDelete=[rabbit_plugins, rabbit_misc, rabbit_file].
 
-ListMod=ListModAll++ListModeAdd.
+ListMod=(ListModAll++ListModeAdd)--ListModeDelete.
 %lists:member(rabbit_env,  ListMod).
 DbgList = lists:foldl(fun(X, Sum) -> [{X, '_', fun(_) -> return_trace() end}] ++ Sum end, [], ListMod).
 {ok, Dev} = file:open(FileName,[write]).
