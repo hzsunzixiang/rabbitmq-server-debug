411adaa28a984d7694262e8b0875758c1e29ceef
haichengsun <haichengsun123@163.com>
2023-11-06 04:47:59 -0500

    icommit

--- ranch/ranch_example/examples/tcp_echo/src/echo_protocol.erl
+++ ranch/ranch_example/examples/tcp_echo/src/echo_protocol.erl
@@ -22,3 +22,39 @@ loop(Socket, Transport) ->
 		_ ->
 			ok = Transport:close(Socket)
 	end.
+
+%handshake(State=#state{ref=Ref, transport=Transport, handshake_timeout=HandshakeTimeout,
+%		max_conns=MaxConns, alarms=Alarms0}, CurConns, NbChildren, Sleepers, To, Socket, SupPid, ProtocolPid) ->
+%	case Transport:controlling_process(Socket, ProtocolPid) of
+%		ok ->
+%			ProtocolPid ! {handshake, Ref, Transport, Socket, HandshakeTimeout},
+%			put(SupPid, active),
+%			CurConns2 = CurConns + 1,
+%			Sleepers2 = if CurConns2 < MaxConns ->
+%					To ! self(),
+%					Sleepers;
+%				true ->
+%					[To|Sleepers]
+%			end,
+%			Alarms1 = trigger_alarms(Ref, Alarms0, CurConns2),
+%			loop(State#state{alarms=Alarms1}, CurConns2, NbChildren + 1, Sleepers2);
+%		{error, _} ->
+%			Transport:close(Socket),
+%			%% Only kill the supervised pid, because the connection's pid,
+%			%% when different, is supposed to be sitting under it and linked.
+%			exit(SupPid, kill),
+%			To ! self(),
+%			loop(State, CurConns, NbChildren, Sleepers)
+%	end.
+%
+%
+%handshake1(Ref, Opts) ->
+%	receive {handshake, Ref, Transport, CSocket, Timeout} ->
+%		Handshake = handshake_transport(Transport, handshake, CSocket, Opts, Timeout),
+%		handshake_result(Handshake, Ref, Transport, CSocket, Timeout)
+%	end.
+%
+%handshake_transport(Transport, Fun, CSocket, undefined, Timeout) ->
+%	Transport:Fun(CSocket, Timeout);
+%handshake_transport(Transport, Fun, CSocket, {opts, Opts}, Timeout) ->
+%	Transport:Fun(CSocket, Opts, Timeout).
