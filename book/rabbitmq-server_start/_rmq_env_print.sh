fc48d4cfb1d24677d0337ad6902f56e34c17e52c
sunzixiang <haichengsun123@163.com>
2025-01-21 21:25:39 -0500

    systemd

--- book/rabbitmq-server_start/_rmq_env_print.sh
+++ book/rabbitmq-server_start/_rmq_env_print.sh
@@ -33,10 +33,18 @@ _rmq_env_pwarn()
 	    _rmq_env_print '[warning]' "$@"
 }
 
- _rmq_env_print '[error]' "$@"
+_rmq_env_print '[error]' "$@"
 
 _rmq_env_pwarn 'RABBITMQ_PID_FILE was already set by the init script to:' \
         "saved_RABBITMQ_PID_FILE" \
         'The value set in rabbitmq-env.conf is ignored because it would break the init script.'
 
 
+
+#[ericksun@rabbitmq4-1:~/workspace/rabbitmq-server-debug/book/rabbitmq-server_start] (main *%)$ bash _rmq_env_print.sh  xxx yy zz
+#2025-01-21 21:10:00 [error] xxx
+#                            yy
+#                            zz
+#2025-01-21 21:10:00 [warning] RABBITMQ_PID_FILE was already set by the init script to:
+#                              saved_RABBITMQ_PID_FILE
+#                              The value set in rabbitmq-env.conf is ignored because it would break the init script.
